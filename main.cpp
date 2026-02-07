025e44bba16c7a4ff8600f393976f1b39c946543
Jhony279 <lordjhony732@gmail.comm>
2026-02-06 21:00:11 -0800

    Created the function to prompt user for the restruants information

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 // Function Prototypes:
 
-void getRestruantInfor();
+void getRestruantInfo();
 
 struct Restruant{
     string name;
@@ -19,12 +19,26 @@ struct Restruant{
 // arguments: 
 // returns: 
 int main() {
+    getRestruantInfo();
     return 0;
 }
 
 // Function Definitions:
 
-
+void getRestruantInfo(){
+    Restruant restruant;
+    cout << "------------------- Restruant Information -------------------" << endl;
+    cout << "Enter the name of the restruant: ";
+    cin >> restruant.name;
+    cout << "Enter the adress of the restruant: ";
+    cin >> restruant.adress;
+    cout << "Enter the phone number of the restruant: ";
+    cin >> restruant.storeNumber;
+    cout << "Enter the amount of days the restruant is open: ";
+    cin >> restruant.amountOfDaysOpen;
+    cout << "Does the restruant remain open during late hours?: ";
+    cin >> restruant.openLate;
+}
 
 // Reminder lines must be 80 characters or less, no more than one empty line, no magic numbers,
 // and Reminder to comment code where neccessary.
