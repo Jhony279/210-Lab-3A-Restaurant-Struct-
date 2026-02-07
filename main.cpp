059d93981dda16fdb49cc6c01f7e021e5ac7f743
Jhony279 <lordjhony732@gmail.comm>
2026-02-06 21:23:32 -0800

    removing and debugging any flaws from the getRestruantInfo function

--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@ struct Restruant{
     string name;
     string adress;
     float storeNumber;
-    float amountOfDaysOpen;
+    char amountOfDaysOpen;
     bool openLate;
 };
 
@@ -27,17 +27,19 @@ int main() {
 
 void getRestruantInfo(){
     Restruant restruant;
-    cout << "------------------- Restruant Information -------------------" << endl;
+    
+    cout << "-------------- Restruant Information --------------" << endl;
     cout << "Enter the name of the restruant: ";
     getline(cin, restruant.name);
     cout << "Enter the adress of the restruant: ";
     getline(cin, restruant.adress);
-    cout << "Enter the phone number of the restruant: ";
-    cin >> restruant.storeNumber;
+    cout << "Does the restruant stay open late? (Enter true or false): ";
+    cin >> boolalpha >> restruant.openLate;
     cout << "Enter the amount of days the restruant is open: ";
     cin >> restruant.amountOfDaysOpen;
-    cout << "Does the restruant remain open during late hours?: ";
-    cin >> restruant.openLate;
+    cin.ignore(1000, '\n'); // Clear out remaining input before requesting next 
+    cout << "Enter the phone number of the restruant: ";
+    cin >> restruant.storeNumber;
 }
 
 // Reminder lines must be 80 characters or less, no more than one empty line, no magic numbers,
